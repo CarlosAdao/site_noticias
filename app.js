ccbc8dfa6d2d7057c412eb640389e87f8794258b
carlos_adao <jose.carlos.adao@hotmail.com>
2021-05-18 22:10:49 -0300

    adicionando o modulo do ejs

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 
+//Adicionando o novo engine de view EJS
+app.set('view engine', 'ejs');
+
+
 //Trata as requisições com express
 app.get('/', function(req, res){
-    html = "<html><body>Retornando um portal de noticias</body></html>"
+    html = "<html><body>Home</body></html>"
     res.send(html)
 })
+
+//Trata as requisições com express
+app.get('/noticias', function(req, res){
+    html = "<html><body>Painel de noticias</body></html>"
+    res.send(html)
+})
+
 
 
 app.listen(3000, function(){
